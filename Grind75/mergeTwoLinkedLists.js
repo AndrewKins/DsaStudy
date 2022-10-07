1d97c0d1ec474b9ea5b182e7abb2dc82730951ba
AndrewKins <andrewm.enoe@gmail.com>
2022-10-07 13:48:16 -0400

    added another problem

--- Grind75/mergeTwoLinkedLists.js
+++ Grind75/mergeTwoLinkedLists.js
@@ -6,6 +6,8 @@ P
     -Merge the two lists into one sorted list
     The list should be made by splicing together the nodes of the first two lists.
     Return the head of the merged linked list.
+
+    Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 E
     List 1 (2,6,8) List 2 (4,3,9)
     output: 2,3,4,6,8,9
@@ -15,3 +17,7 @@ A
     
 C
 */
+
+var mergeTwoLists = function(list1, list2) {
+    
+};
