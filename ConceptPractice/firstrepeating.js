0942c7e5bb0c5bceae0dfe837205027973a770e3
AndrewKins <andrewm.enoe@gmail.com>
2022-12-03 16:22:49 -0500

    update string

--- ConceptPractice/firstrepeating.js
+++ ConceptPractice/firstrepeating.js
@@ -1,19 +1,90 @@
-const firstRepeat = (str) => {
-    let newString = str.split('')
-    const charFreq = {}
-    for(let ele of newString){
-        if(!charFreq[ele]){
-            charFreq[ele] = 1
-        } else{
-            charFreq[ele] += 1
-        }
-            if(Object.values(charFreq) === 2){
-                return charFreq
-            }
-    }
+// const firstRepeat = (str) => {
+//     let newString = str.split('')
+//     const charFreq = {}
+//     for(let ele of newString){
+//         if(!charFreq[ele]){
+//            return charFreq
+//         } else{
+//             charFreq[ele] += 1
+//         }
+//         console.log(charFreq)
+//     }
 
-}
-console.log(firstRepeat('reuben'))
+// }
+// console.log(firstRepeat('reuben'))
 // console.log(firstRepeat('anne'))
 // console.log(firstRepeat('restaurant'))
 // console.log(firstRepeat('paul'))
+
+
+/*
+Problem 
+input: string 
+output: string 
+return the non repeating character, if there none, return "_"
+
+examples:
+input: "abcd" 
+a, b, c, d 
+output: "a"
+
+input: "aaaabb"
+output: "_"
+
+input: "beepbeepboopboop"
+output: "_"
+
+DS
+Stirng, 
+object: key value pairs
+
+ 
+{
+    key: value,
+    "a": 2, 
+    "b": 2, 
+    "c": 1
+}
+
+"aabbc"
+     ^
+ 
+Algorithm
+declare an empty object 
+
+iterate over the given string 
+for each character: 
+    is it a property in the obj? 
+        if it is: increment value 
+        if it is not: add the charcater as a propety to the object, set it's value to 1 
+        
+iterate over the string to find the first non-repeating char:
+for each character in given string: 
+    if the charcater has a value of 1 in the object:
+    return the character 
+    
+
+return "_"
+
+ */
+function solution(str) {
+    let freq = {}
+    
+    for(let i = 0; i < str.length; i++){
+        let char = str[i]
+        if(freq[char]){
+            freq[char] += 1
+        }else{
+            freq[char] = 1
+        }
+    }
+    
+    for(let i = 0; i < str.length; i++){
+        let char = str[i]
+        if(freq[char] === 1){
+            return char
+        }
+    }
+    
+    return "_"
+}
--- ConceptPractice/string.js
+++ ConceptPractice/string.js
@@ -1,22 +1,50 @@
-function cleanNumbers(str){
-    let newNumber = str.replace(/\D/g, "")
-    console.log(newNumber)
+//String object is used to manipulate a sequence of characters.
 
-    if(newNumber.length < 10 || newNumber.length > 11){
-        return '0000000000'
-    }
-    if(newNumber.length === 11){
-        return string = newNumber.substring(1)
+/*Some of the most-used operations on strings 
+    -check their length, 
+    -build and concatenate them using the + & += 
+    -check for the existence or location of substrings with indexOf()
+    -extracting substrings with the substring() method.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function cleanNumbers(str){
+   let newNumber = str.replace(/\D/g, "")
+     console.log(newNumber
+     if(newNumber.length < 10 || newNumber.length > 11){
+         return '0000000000'
      }
-    if(newNumber.length === 11 && newNumber.charAt() !== 11){
-        return '0000000000'
-    }
-    else {
-        return newNumber
-    }
-}
-
-console.log(cleanNumbers('919-621-9 388') === '9196219388')
-console.log(cleanNumbers('919-621-') === '0000000000')
-console.log(cleanNumbers('2-919-621-9388') === '0000000000')
-console.log(cleanNumbers('+1 (919) 621-9388') === '9196219388')
+     if(newNumber.length === 11){
+         return string = newNumber.substring(1)
+      }
+     if(newNumber.length === 11 && newNumber.charAt() !== 11){
+         return '0000000000'
+     }
+     else {
+         return newNumber
+     }
+ 
+ console.log(cleanNumbers('919-621-9 388') === '9196219388')
+ console.log(cleanNumbers('919-621-') === '0000000000')
+ console.log(cleanNumbers('2-919-621-9388') === '0000000000')
+ console.log(cleanNumbers('+1 (919) 621-9388') === '9196219388')
+ */
