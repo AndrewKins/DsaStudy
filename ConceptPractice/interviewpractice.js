67bfa1e17ea8e5056ddd631eca61bd2d875b03af
AndrewKins <andrewm.enoe@gmail.com>
2022-12-12 15:45:00 -0500

    added

---
+++ ConceptPractice/interviewpractice.js
@@ -0,0 +1,40 @@
+// const greeting = (string) => {
+//     return `Hello, ${string}, How are you?`
+// }
+
+// console.log(greeting('Andrew'))
+
+// const getRating = (title,review) => {
+//     if(review === 'bad' || review === 'awful') console.log('*')
+    
+//     else if(review === 'good') console.log('**')
+
+//     else if(review === 'great') console.log('***')
+    
+//     else(console.log('???'))
+//     console.log(`Movie:${title}`)
+// }
+
+// console.log(getRating('titanic','bad'))
+
+// const hideAndSeekCounter = (num) => {
+// for(let i = 1; i <= num; i += 1){
+//     if(i < num) console.log(`${i}...`)
+//     if(i === num) console.log(`${i}!`)
+// }
+// console.log("Ready or not, here I come!")
+// }
+// console.log(hideAndSeekCounter(6))
+
+// const cheer = (word) => {
+//     for(let i = 0 ; i < word.length; i += 1){
+//         console.log(`Give me a ${word[i].toUpperCase()}!`)
+//     }
+//     console.log('Whats it spell?')
+//     console.log(`${word.toUpperCase()}!`,`${word.toUpperCase()}!`,`${word.toUpperCase()}!`)
+// }
+// console.log(cheer('win'))
+
+const betterCheet = () => {
+    
+}
--- ConceptPractice/string.js
+++ ConceptPractice/string.js
@@ -7,6 +7,37 @@
     -extracting substrings with the substring() method.
 */
 
+/*
+----Access individual character in a string.----
+
+    "stringName".charAt(indexlocation)  
+
+    "stringName"[indexlocation] 
+
+----Return unicode of a character in a string.----
+    "stringName".charCodeAt(indexlocation)
+
+----Return the index number of a character/substring found in string----
+    stringName.indexOf(searchString)
+    stringName.indexOf(searchString, position(where to start))
+    stringName.lastIndexOf(substr, [start]) - Starts at the end of the string
+    stringName.match
+*/
+
+
+
+const fullName = 'Andrew'
+const checkStringLocation = (str) => {
+    // return str.charAt(2) //expected output is d
+    // return str[2] //expected output is d again
+    // return str.charCodeAt() //expected output unicode
+    return fullName.indexOf('ndr') !== -1; //if it returns -1 then its false //can also be used to chech occurences
+}
+
+console.log(checkStringLocation(fullName))
+
+
+
 
 
 
