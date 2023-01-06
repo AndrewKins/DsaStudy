a6b52d0fda8087bc66e98220a0eff5825a9fecf6
AndrewKins <andrewm.enoe@gmail.com>
2023-01-05 20:17:09 -0500

    worked on objects

--- ConceptPractice/objects.js
+++ ConceptPractice/objects.js
@@ -1,59 +1,69 @@
-//Definition
+/*
+Definition
 
-//objects are a collection of related values that stores multiple pieces of data about an entity.
-//Contains key value pairs.
-//Data is stored in a section of memory called heap 
+objects are a collection of related values that stores multiple pieces of data about an entity.
+Contains key value pairs.
+Data is stored in a section of memory called heap 
 
-//Objects can be initialized in 3 ways
+Objects can be initialized in 3 ways
 
-//let obj = {}
-//const models = new Obj()
-
-//Access properties of an Obj
-
-//Bracket Notation
-   // myObj['keyname']
-    //use if key is a Number
-    //string beginning with non alphanumeric character
-//Dot Notation
-    //myObj.keyname
-    //can only use if key is character is a value
-
-// const user = {
-//     realname: 'andrew',
-//     age: '25',
-//     techstack: ['javascript','react','express','node']
-// }
-// // console.log(user)
-// // console.log(user['age']) //use brackets with strings to target value of the key
-// // console.log(user.techstack[1])
-// // console.log(user.techstack.length)
-// // console.log(user['techstack'][1])
-
-// for(let ele in user){
-//     // console.log(ele, 'testingg') //ele are the keys of the object
-//     // console.log(user[ele], 'what is this') //bracket notation targets values of the keys
-//     // console.log(user[ele], ele)
-//     console.log(user.ele)
-// } 
+let obj = {}
+const models = new Obj()
+*/
 
 
-// const objTest = (arr) => {
-//     let hashMap = {}
-//     for(let i = 0; i < arr.length; i += 1){
-//         if(!hashMap[arr[i]]){
-//             hashMap[arr[i]] = 1
-//         }else
-//             hashMap[arr[i]]++
-//             occurences.push(hashMap[arr])
-//             // console.log(hashMap, 'testing')
-//     }
-//     console.log(hashMap)
-// }
+/*
+Access properties of an Obj
+
+Bracket Notation
+
+   myObj['keyname']
+    use if key is a Number
+    string beginning with non alphanumeric character
+
+Dot Notation
+
+    myObj.keyname
+    can only use if key is character is a value
+
+const user = {
+    realname: 'andrew',
+    age: '25',
+    techstack: ['javascript','react','express','node']
+}
+// console.log(user)
+// console.log(user['age']) //use brackets with strings to target value of the key
+// console.log(user.techstack[1])
+// console.log(user.techstack.length)
+// console.log(user['techstack'][1])
+*/
+
+//Loop through an object
+
+for(let ele in user){
+    // console.log(ele, 'testingg') //ele are the keys of the object
+    // console.log(user[ele], 'what is this') //bracket notation targets values of the keys
+    // console.log(user[ele], ele)
+    console.log(user.ele)
+} 
+
+
+const objTest = (arr) => {
+    let hashMap = {}
+    for(let i = 0; i < arr.length; i += 1){
+        if(!hashMap[arr[i]]){
+            hashMap[arr[i]] = 1
+        }else
+            hashMap[arr[i]]++
+            occurences.push(hashMap[arr])
+            // console.log(hashMap, 'testing')
+    }
+    console.log(hashMap)
+}
 
 
 
-// console.log(objTest([1,2,5,5,7,3,9,10,1]))
+console.log(objTest([1,2,5,5,7,3,9,10,1]))
  
 let cat = {
     name: "Mary",
