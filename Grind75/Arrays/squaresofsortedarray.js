117567cc2d327ae5ad0caee4f7aa23a1b3b7a784
AndrewKins <andrewm.enoe@gmail.com>
2023-01-11 14:13:48 -0500

    updated problem

---
+++ Grind75/Arrays/squaresofsortedarray.js
@@ -0,0 +1,20 @@
+const sortedSquares = (nums) => {
+    let left = 0,
+        right = nums.length - 1,
+        position = nums.length - 1,
+        result = [];
+    while(left <= right){
+        if(nums[left] ** 2 > nums[right] ** 2){
+            result[position] = nums[left] ** 2
+            position--;
+            left++;
+        } else {
+            result[position] = nums[right] ** 2;
+            position--;
+            right--;
+        }
+    }
+    return result;
+};
+
+console.log(sortedSquares([-4,-1,0,3,10]))
